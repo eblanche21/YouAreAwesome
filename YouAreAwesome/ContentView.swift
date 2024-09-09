6639c5d773593129ea891bd9221f15978c1cc51b
Ethan Blanche <blancheq@bc.edu>
2024-09-09 17:55:50 -0400

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Ethan Blanche on 8/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var messageString = ""
+    @State private var imageName = ""
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
+    
+    var body: some View {
+        
+        VStack {
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .padding()
+            
+            Spacer()
+            
+            Button("Show Message") {
+                let messages = ["You Are Awesome!",
+                                "You Are Great!",
+                                "You Are Fantastic!",
+                                "Fabulous? That's You!"]
+                
+                //generate a random messageNumber to use as an index
+                // if messageNumber == lastMessageNumber {
+                //     keep generating a new random messageNumber
+                //  until you get a messageNumber != lastMessageNumber
+                // Set messageString to messages[messageNumber]
+                // update the lastMessageNumber with messageNumber
+        
+                var messageNumber: Int
+               repeat {
+                    messageNumber = Int.random(in: 0...messages.count-1)
+               } while messageNumber == lastMessageNumber
+                messageString = messages[messageNumber]
+                lastMessageNumber = messageNumber
+                
+                var imageNumber: Int
+                repeat {
+                    imageNumber = Int.random(in: 0...9)
+                } while imageNumber == lastImageNumber
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
+            }
+            .buttonStyle(.borderedProminent)
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
